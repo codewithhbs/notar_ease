1b060884196851f58f58ae75b63ac45fa0fd64a6
Coderhitesh <hiteshy468@gmail.com>
2026-02-17 15:31:28 +0530

    add

--- backend/routes/meetingDemoRoutes.js
+++ backend/routes/meetingDemoRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { authenticateAccessToken, authorizeRoles } = require("../utils/jwtUtil");
 const DemoMeeting = require("../controllers/meetingDemo.controller");
 
-router.post('/create-demo-meeting', authenticateAccessToken, DemoMeeting.createMeetingDemo);
+router.post('/create-demo-meeting', DemoMeeting.createMeetingDemo);
 router.get('/get-all-demo-meetings', authenticateAccessToken, authorizeRoles('admin'), DemoMeeting.getAllMeetingDemos);
 router.delete('/delete-demo-meeting/:id', authenticateAccessToken, authorizeRoles('admin'), DemoMeeting.deleteDemoMeeting);
 
